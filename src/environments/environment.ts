338c586729551a097ef96bdc0c4781c51b0ef736
Nicolas Cuevas <nicolasgcuevas@gmail.com>
2019-12-17 17:16:30 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-	apiKey: "AIzaSyBDbfWyvLZkENqnig2QMEywKBPagBD8NN8",
-	authDomain: "veterinary-a2fa7.firebaseapp.com",
-	databaseURL: "https://veterinary-a2fa7.firebaseio.com",
-	projectId: "veterinary-a2fa7",
-	storageBucket: "veterinary-a2fa7.appspot.com",
-	messagingSenderId: "897672228085",
-	appId: "1:897672228085:web:6f18427c03a743c5"
+	apiKey: "",
+	authDomain: "",
+	databaseURL: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: ""
   }
 };
 
